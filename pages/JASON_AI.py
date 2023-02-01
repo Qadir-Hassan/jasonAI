7f067c5c91abd059c23cece66b566757ee1927f7
qadirhassan <qadr.hassan333@gmail.com>
2023-02-01 10:53:32 +0500

    final

--- pages/JASON_AI.py
+++ pages/JASON_AI.py
@@ -11,31 +11,31 @@ co = cohere.Client('xGECIS7W6UZvlHvmsoj4ArmhxBpfMlJKg2spNOzr')
 if 'output' not in st.session_state:
     st.session_state['output'] = 'Output:'
 
-def generate_hashtags(input):
+def generate_mobile_details(input):
     if len(input) == 0:
         return None
     response = co.generate( 
-    model='command-xlarge-20221108', 
-    prompt='Given a post, this program will generate relevant hashtags.\n\nPost: Why are there no country songs about software engineering\nHashtag: #softwareengineering #code \n--\nPost: Your soulmate is in the WeWork you decided not to go to\nHashtag: #wework #work \n--\nPost: If shes talking to you once a day im sorry bro thats not flirting that standup\nHashtag: #standup #funny \n--\nPost: {}\nHashtags:'.format(input), 
-    max_tokens=20, 
-    temperature=0.5, 
+    model='6c17f430-f7a9-4e5c-ae36-322ab2f1d903-ft',
+    prompt=f"The Details and specification for this mobile{input} is ",
+    max_tokens=500, 
+    temperature=0.9, 
     k=0, 
-    p=1, 
+    p=0.75, 
     frequency_penalty=0, 
     presence_penalty=0, 
     stop_sequences=["--"], 
     return_likelihoods='NONE') 
     
-    st.session_state['output'] = response.generations[0].text
+    st.session_state['output'] ='Mobile Details and Specification: {}'.format( response.generations[0].text)
     st.balloons()
 
 
-st.title('Hashtag Generator')
-st.subheader('Boilerplate for Co:here, Streamlit, Streamlit Cloud')
-st.write('''This is a simple **Streamlit** app that generates hashtags from a small Post title caption.''')
+st.title('AI Customer Care')
+st.subheader('Jason AI')
+st.write('''This is a simple **Streamlit** app that generates Mobile Model details''')
 
-input = st.text_area('Enter your post title caption here', height=100)
-st.button('Generate Hashtags', on_click = generate_hashtags(input))
+input = st.text_area('Enter your Mobile phone Name and model', height=100)
+st.button('Generate Details', on_click = generate_mobile_details(input))
 st.write(st.session_state.output)
 
 
